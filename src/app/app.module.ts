c4da8c5712ecc9f39a0fd474556d2e7b016580ba
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-03-31 19:21:15 +0530

    Impliment Inline-Style & Inline-Template at time and learn how it works parallel.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { StudentListComponent } from './student-list/student-list.component';
+import { CountryListComponent } from './country-list/country-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
     StudentListComponent,
+    CountryListComponent,
   ],
   imports: [
     BrowserModule,
