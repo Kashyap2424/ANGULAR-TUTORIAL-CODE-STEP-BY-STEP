7cdc384a9fa67f4b62e0ab37f77a4131f640e305
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-16 19:08:12 +0530

    Create one TODO List application in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Material Ui with angular';
+  title = 'TODO Application with angular';
+
+  list: any[] = [];
+
+  addTodo(item: string) {
+    this.list.push({ id: this.list.length + 1, name: item });
+    console.log(item, this.list);
+  }
+
+  removeAddedTodo(id: any) {
+    this.list = this.list.filter((todo) => todo.id !== id);
+  }
 }
