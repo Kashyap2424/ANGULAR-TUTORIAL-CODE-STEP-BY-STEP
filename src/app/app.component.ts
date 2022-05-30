af2a7efc60b7402d966c758dfee8062c9450297c
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-30 19:26:53 +0530

    Learn what is directives & how to create one custom directives in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,5 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular Reactive Form Validatoins';
-
-  loginForm = new FormGroup({
-    emailAddress: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      ),
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.pattern(
-        /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
-      ),
-    ]),
-  });
-
-  loginUser() {
-    console.warn(this.loginForm.value);
-  }
-
-  get emailAddress() {
-    return this.loginForm.get('emailAddress');
-  }
-  get password() {
-    return this.loginForm.get('password');
-  }
+  title = 'Angular Directives & Custome Directives';
 }
