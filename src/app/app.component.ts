259a20e6c525b604ffcd3d75cf4d519055810eef
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-30 10:57:08 +0530

    Create one simpale todo application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'TODO Application with angular';
+  title = 'Passing data to a child component';
 
-  list: any[] = [];
+  childComponentTitle = 'This is the title from the child component';
 
-  addTodo(item: string) {
-    this.list.push({ id: this.list.length + 1, name: item });
-    console.log(item, this.list);
-  }
+  childComponentTitleNext =
+    'This is the title from the child component - Updated ! ⁄˚◡˚⁄';
 
-  removeAddedTodo(id: any) {
-    this.list = this.list.filter((todo) => todo.id !== id);
+  updateChildComponentData() {
+    this.childComponentTitle = this.childComponentTitleNext;
   }
 }
