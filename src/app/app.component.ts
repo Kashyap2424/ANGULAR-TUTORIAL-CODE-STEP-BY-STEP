3c22a1a768f45002f9b56933350edffb1b08f93d
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-30 17:49:22 +0530

    Learn how to add validation in reactive forms in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, enableProdMode } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +7,31 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular Reactive Form';
+  title = 'Angular Reactive Form Validatoins';
 
   loginForm = new FormGroup({
-    emailAddress: new FormControl(''),
-    password: new FormControl(''),
+    emailAddress: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+      ),
+    ]),
   });
 
   loginUser() {
     console.warn(this.loginForm.value);
+  }
+
+  get emailAddress() {
+    return this.loginForm.get('emailAddress');
+  }
+  get password() {
+    return this.loginForm.get('password');
   }
 }
