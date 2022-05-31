ce6aad4f0ba3862ba8f8d9ffdef383912f020d9b
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-05-31 19:44:53 +0530

    Learn how to work service in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutCompanyComponent } from './about-company/about-company.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'about',
-    component: AboutComponent,
-    children: [
-      { path: 'company', component: AboutCompanyComponent },
-      { path: 'me', component: AboutMeComponent },
-    ],
-  },
-  {
-    path: 'user/:userId',
-    component: UserComponent,
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,23 +9,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { UserComponent } from './user/user.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AboutCompanyComponent } from './about-company/about-company.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    UserComponent,
-    PageNotFoundComponent,
-    AboutCompanyComponent,
-    AboutMeComponent,
-  ],
+  declarations: [AppComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
