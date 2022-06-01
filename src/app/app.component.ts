97806ef2d546394c96d11b7703d7a61c1941f277
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-01 17:01:18 +0530

    Create our first unit test case in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,36 +9,5 @@ import {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular Lazy Loading Components';
-
-  constructor(
-    private viewContainerRef: ViewContainerRef,
-    private componentFactoryResolver: ComponentFactoryResolver
-  ) {}
-
-  async loadAdminList() {
-    this.viewContainerRef.clear();
-    const { AdminListComponent } = await import(
-      './admin-list/admin-list.component'
-    );
-
-    this.viewContainerRef.createComponent(
-      this.componentFactoryResolver.resolveComponentFactory(
-        AdminListComponent // <-- Component to load
-      ) // <-- Component to load
-    );
-  }
-
-  async loadUserList() {
-    this.viewContainerRef.clear();
-    const { UserListComponent } = await import(
-      './user-list/user-list.component'
-    );
-
-    this.viewContainerRef.createComponent(
-      this.componentFactoryResolver.resolveComponentFactory(
-        UserListComponent // <-- Component to load
-      ) // <-- Component to load
-    );
-  }
+  title = 'Angular First Unit Test Case';
 }
