a76e10571450c2fa668b895e0526ab974de47af3
Kashyap Kapdi <kapdikashyap2003@gmail.com>
2022-06-01 15:55:44 +0530

    Learn how to work a routing module in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
-import userModeldataType from './model/user-model';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular Model & Interface';
-  getData() {
-    const data: userModeldataType = {
-      id: 1,
-      name: 'Manish',
-      indian: true,
-      address: {
-        city: 'Mumbai',
-        state: 'Maharashtra',
-      },
-    }; // dataType interface
-
-    return data;
-  }
+  title = 'Angular Routing Module Example';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatBadgeModule } from '@angular/material/badge';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    BrowserAnimationsModule,
-    MatSliderModule,
-    MatButtonModule,
-    MatBadgeModule,
-    FormsModule,
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, AdminModule],
   providers: [],
   bootstrap: [AppComponent],
 })
